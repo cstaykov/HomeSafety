af202934c99dcdbb2f09e77427a74c933c6e6f74
Jean Tu <jeanjtu@gmail.com>
2018-11-11 03:34:10 -0500

    API CALL WORKS WITH HARDCODED VALUE

--- js/index.js
+++ js/index.js
@@ -5,7 +5,21 @@ function insertDiv() {
 
   // Get the reference node
   var referenceNode = document.getElementById('gac_scont');
+  let options = {
+    "url": "https://apis.solarialabs.com/shine/v1/total-home-scores/reports?address=65%20Seaport%20Blvd%2C%20Boston%20MA%2C%20USA&apikey=YFYHd0eSblGWUtBTYBIkGCqg9z27nZra",
+    "method": "GET",
+    "processData": false
+  }
+  $.ajax(options).done((response)=>{
+    console.log(response)
+  })
+  // var request = new XMLHTTPRequest()
+  // request.open('GET',"https://apis.solarialabs.com/shine/v1/total-home-scores/reports?address=65%20Seaport%20Blvd%2C%20Boston%20MA%2C%20USA&apikey=YFYHd0eSblGWUtBTYBIkGCqg9z27nZra", false );
 
+    console.log(options);
+    console.log('done');
+
+  //document.write(options)
   // Insert the new node before the reference node
   referenceNode.after(newNode);
 }
