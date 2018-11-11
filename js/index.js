4e840a1f07d97cd30178676992728c0b8668e1a4
Steven Xu <steven.xu1589@gmail.com>
2018-11-11 08:10:10 -0500

    Add dynamically grabbed address and make API call with parsed address

--- js/index.js
+++ js/index.js
@@ -6,22 +6,51 @@ function insertDiv() {
     //street[i].textContent
     //$("steven").text(street[i].textContent);
 
-  let options = {
-    "url": "https://apis.solarialabs.com/shine/v1/total-home-scores/reports?address=65%20Seaport%20Blvd%2C%20Boston%20MA%2C%20USA&apikey=YFYHd0eSblGWUtBTYBIkGCqg9z27nZra",
-    "method": "GET",
-    "processData": false
-  }
-  $.ajax(options).done((response) => {
-    alert(response.totalHomeScores.quiet.value)
     var referenceNodes = document.getElementsByClassName('pricing resp-module')
     var street = document.getElementsByClassName('p-street-address')
+    var city = document.getElementsByClassName('p-locality')
+    var state = document.getElementsByClassName('p-region')
+
+
+
     for (i = 0; i < referenceNodes.length; i++)  {
+
+      var streetText = street[i].textContent.replace(/\u00a0/g, "%20");
+      var cityText = city[i].textContent.replace(/\u00a0/g, "%20");
+      var stateText = state[i].textContent.replace(/\u00a0/g, "%20");
+
+      streetText = streetText.replace(/ /g, "%20");
+      cityText = cityText.replace(/ /g, "%20");
+      stateText = stateText.replace(/ /g, "");
+
+      streetText = streetText.replace(",", "");
+      cityText = cityText.replace(",", "");
+      stateText = stateText.replace(",", "");
+
+      address = streetText + cityText + stateText
+      // alert("address=" + address + "%2C%20USA")
+
+
+      // alert(i)
+
       var newNode = document.createElement('div');
       newNode.id = "steven"
       referenceNodes[i].append(newNode)
-      newNode.textContent = response.totalHomeScores.quiet.value.toString()
-    }
-  })
+      newNode.textContent = address
+
+      let options = {
+        "url": "https://apis.solarialabs.com/shine/v1/total-home-scores/reports?address=" + address + "%2C%20USA&apikey=YFYHd0eSblGWUtBTYBIkGCqg9z27nZra",
+        "method": "GET",
+        "processData": false
+      }
+      $.ajax(options).done((response) => {
+        // alert(i + " INSIDE AJAX")
+        // alert(response.totalHomeScores.quiet.value)
+
+        newNode.textContent = response.totalHomeScores.quiet.value.toString()
+      })
+
+  }
 
 
 
