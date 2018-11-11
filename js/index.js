db49b36b880d698e5da3e6bf81ac531ee5b3534d
Steven Xu <steven.xu1589@gmail.com>
2018-11-11 07:27:11 -0500

    hardcode address for value

--- js/index.js
+++ js/index.js
@@ -3,31 +3,29 @@ function insertDiv() {
 
   // Get the reference node
 
-  
+    //street[i].textContent
+    //$("steven").text(street[i].textContent);
+
   let options = {
     "url": "https://apis.solarialabs.com/shine/v1/total-home-scores/reports?address=65%20Seaport%20Blvd%2C%20Boston%20MA%2C%20USA&apikey=YFYHd0eSblGWUtBTYBIkGCqg9z27nZra",
     "method": "GET",
     "processData": false
   }
-  $.ajax(options).done((response)=>{
-    console.log(response)
+  $.ajax(options).done((response) => {
+    alert(response.totalHomeScores.quiet.value)
+    var referenceNodes = document.getElementsByClassName('pricing resp-module')
+    var street = document.getElementsByClassName('p-street-address')
+    for (i = 0; i < referenceNodes.length; i++)  {
+      var newNode = document.createElement('div');
+      newNode.id = "steven"
+      referenceNodes[i].append(newNode)
+      newNode.textContent = response.totalHomeScores.quiet.value.toString()
+    }
   })
-  
-
-    console.log(options);
-    console.log('done');
 
 
 
-  var referenceNodes = document.getElementsByClassName('pricing resp-module')
-  console.log('a ting:')
-  console.log(referenceNodes)
 
-  for (i = 0; i < referenceNodes.length; i++)  {
-    var newNode = document.createElement('div');
-    newNode.id = "steven"
-    referenceNodes[i].append(newNode)
-  }
 
 
 }
